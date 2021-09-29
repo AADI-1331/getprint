b6c79202474679ad5ec4239e50f1548f66526ac8
Aaditya Jaiswal <78849620+AADI-1331@users.noreply.github.com>
2021-09-29 11:25:04 +0530

    Create getprint.py

---
+++ getprint.py
@@ -0,0 +1,17 @@
+# Hints:
+#Use __init__ method to construct some parameters
+
+
+class InputOutString(object):
+    def __init__(self):
+        self.s = ""
+
+    def getString(self):
+        self.s = raw_input()
+
+    def printString(self):
+        print self.s.upper()
+
+strObj = InputOutString()
+strObj.getString()
+strObj.printString()
